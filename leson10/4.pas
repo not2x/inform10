3e07c6ce8397e450a6c8c5e6d59c3093d8980254
not2x <91423699+not2x@users.noreply.github.com>
2022-10-22 18:27:04 +0300

    Added solutions for leson 10

---
+++ leson10/4.pas
@@ -0,0 +1,26 @@
+﻿program z4;
+var 
+  a:array[1..10] of integer;
+  temp: integer;
+begin
+  writeln('');
+  for var i:=1 to 10 do a[i]:=Random(10)+1;
+
+  write('Исходный массив: ');
+  for var i:=1 to 10 do write(a[i], ' ');
+  writeln;
+ 
+ 
+  for var i:=1 to 10 do
+  begin
+    if i mod 2 <> 0 then
+    begin
+      temp:=a[i+1];
+      a[i+1]:=a[i];
+      a[i]:= temp;
+    end;
+  end;
+  write('Преобразованный массив: ');
+  for var i:=1 to 10 do write(a[i], ' ');
+  
+end.
---
+++ leson10/5.pas
@@ -0,0 +1,27 @@
+﻿program z3;
+var 
+  a:array[1..10] of integer;
+  min, imin: integer;
+begin
+  writeln('');
+  for var i:=1 to 10 do a[i]:=Random(11);
+
+  write('Исходный массив: ');
+  for var i:=1 to 10 do write(a[i], ' ');
+  writeln;
+  imin:=1;
+  min:=a[1];
+  
+  for var i:=2 to 10 do
+  begin
+    if a[i] < min then
+    begin
+      imin:=i; min:= a[i];
+    end;
+  end;
+  a[imin]:=a[1];
+  a[1]:=min;
+  write('Преобразованный массив: ');
+  for var i:=1 to 10 do write(a[i], ' ');
+  
+end.
